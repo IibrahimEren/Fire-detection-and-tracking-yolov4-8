d7c2839f80f619cc69988177a40d279ebe714a4b
İbrahim <87008174+IibrahimEren@users.noreply.github.com>
2023-05-03 13:17:16 +0300

    Update Combine.py

--- Combine.py
+++ Combine.py
@@ -1,3 +1,5 @@
+# _*_ coding: utf-8 _*_
+
 import cv2
 
 from pyfirmata import Arduino, SERVO, OUTPUT
@@ -19,7 +21,6 @@ from time import sleep
 # Aleve yönelik hareket yapıldıktan sonra görüş açısında çıkan alev için tarama fonksiyonu yazılması gerekir
 
 wCam, hCam = 1280, 720
-# Setup For arduino
 
 port = 'COM3'
 pin1 = 3  # X
@@ -29,17 +30,18 @@ pinPipe = 6  # For water motor
 board = Arduino(port)
 MAX_artis = 45
 
-start_pos = 90
-newX = start_pos  # for the first frame
-# sonrasında newY de olmalı
+start_posX = 90
+start_posY = 0
+newX = start_posX  # for the first frame
+newY = start_posY
 
 board.digital[pin2].mode = SERVO  # Y
 board.digital[pin1].mode = SERVO  # X
-# board.digital[pinTest].mode = OUTPUT  # LED
+board.digital[pinTest].mode = OUTPUT  # LED
 board.digital[pinPipe].mode = OUTPUT  # MOTOR
 
 # SET relay to start position
-board.digital[pinPipe].write(1)  # bir olduğunda kapalı bir şekilde başlatıyor
+board.digital[pinPipe].write(0)  # bir olduğunda kapalı bir şekilde başlatıyor
 board.digital[pinTest].write(0)
 
 
@@ -51,17 +53,18 @@ def rotateservo(pin, angle):
 
 
 # SET servos to start position
-rotateservo(pin1, start_pos)
-rotateservo(pin2, start_pos)
+rotateservo(pin1, start_posX)
+rotateservo(pin2, start_posY)
+
 
 # Hedef görmediği anlarda etrafı taraması için fonksiyon
-# def scanArea(pin, current):
-#     for i in range(current, 180):
-#         for i in range(180, 0):
-#             board.digital[pin].write(i)
-#             # Hedef gördüğü an döngüden çıkması için if bloğu
-#             if bool == True:
-#                 break
+def scanArea(pin1, current):
+    for i in range(current, 180):
+        for i in range(180, 0):
+            board.digital[pin1].write(i)
+            # Hedef gördüğü an döngüden çıkması için if bloğu
+            if len(face) != 0:
+                break
 
 
 # capture the video from camera
@@ -72,10 +75,17 @@ cap.set(4, hCam)
 face_cascade = cv2.CascadeClassifier('dnn_model\\face.xml')
 
 while True:
+    # while True:
+    #     value = input("bla")
+    #     rotateservo(pin1,value)
+
     ret1, frames = cap.read()
     gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
 
-    face = faces_cascade = face_cascade.detectMultiScale(gray, 1.1, 5)
+    face = faces_cascade = face_cascade.detectMultiScale(gray, 1.1, 15)
+    # Eğer algılanan görüntüde yüz/ateş yok ise tarama fonksiyonu çalışsın
+    if len(face) == 0:
+        scanArea(pin1, newX)
 
     for (x, y, w, h) in face:
         cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 0, 255), 2)
@@ -86,8 +96,8 @@ while True:
     center_pointX, center_pointY = wCam / 2, hCam / 2
 
     # Points of center rectangle
-    point1 = int(center_pointX) - 100, int(center_pointY) - 100
-    point2 = int(center_pointX) + 100, int(center_pointY) + 100
+    point1 = int(center_pointX) - 80, int(center_pointY) - 80
+    point2 = int(center_pointX) + 80, int(center_pointY) + 80
     cv2.rectangle(frames, point1, point2, (255, 0, 0), 3)
 
     for (x, y, w, h) in face:
@@ -96,22 +106,25 @@ while True:
         # Algılanan nesnenin merkez noktasına uzaklığı
         merkeze_uzaklikX, merkeze_uzaklikY = regCenterX - center_pointX, regCenterY - center_pointY
 
-        if (center_pointX - 100) < regCenterX < (center_pointX + 100):
+        if (center_pointX - 80) < regCenterX < (center_pointX + 80):
             board.digital[pinPipe].write(0)  # 0 Olduğunda kapatıyor ve motorun çalışmasını sağlıyor
         else:
             board.digital[pinPipe].write(1)
+            # We may want to reduce to amount of increase and decrease
             valueX = merkeze_uzaklikX * MAX_artis / center_pointX
             valueY = merkeze_uzaklikX * MAX_artis / center_pointX
-            newX = newX + valueX/2  # from start point to new point with value increase
+            newX = newX - valueX / 5  # from start point to new point with value increase -
+            newY = newY - valueY / 5  # from start point to new point with value increase -
             if newX > 180:
                 newX = 180
-            #- kısma düşüyor
+            # - kısma düşüyor
             if newX < 10:
                 newX = 0
 
-            print(newX)
+            # print(newX)
+            # Kameranın x ve y koordinatlarındaki takibi
             rotateservo(pin1, newX)
-            # rotateservo(pin2, new)
+            rotateservo(pin2, newY)
 
     # ...
     # Flip the frames for mirror effect
@@ -120,11 +133,11 @@ while True:
     cv2.imshow('Output', frames)
 
     if cv2.waitKey(33) == 27:
-        rotateservo(pin1, start_pos)
-        rotateservo(pin2, start_pos)
-        board.digital[pinPipe].write(1)
-        board.digital[pinTest].write(0)
-        break
+        rotateservo(pin1, start_posX)
+        rotateservo(pin2, start_posY)
+        board.digital[pinPipe].write(0) # it can be change depends on how you wire the role
+        board.digital[pinTest].write(0) # it can be change depends on how you wire the role
+        break                           # I will add the connection diagram for this project later.
 
 cv2.destroyAllWindows()
 cap.release()
